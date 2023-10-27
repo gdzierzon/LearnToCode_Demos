6a46fbac6c65a58f2334c19057aff5649e771d8b
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-10-27 12:18:19 -0600

    added workshop demo

---
+++ Workbook3/06-workshop-demo/scripts/application.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    // const button = document.getElementById("submitButton");
+
+    // button.onclick = submitForm;
+    const theForm = document.getElementById("theForm");
+
+    theForm.onsubmit = submitForm;
+
+}
+
+function submitForm(event) {
+
+    // stop the default form submission
+    event.preventDefault();
+
+    console.log(event);
+    console.log("form submitted")
+}
