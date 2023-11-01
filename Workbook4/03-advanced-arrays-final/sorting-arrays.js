04920a101303346abf46ca9e318e36948c279611
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-11-01 16:31:02 -0600

    added sorting demos

--- Workbook4/03-advanced-arrays-final/sorting-arrays.js
+++ Workbook4/03-advanced-arrays-final/sorting-arrays.js
@@ -1,7 +1,36 @@
 // functions
+function numberSortHelper(first, second) {
+    if(first < second) return -1;
+    else if (first == second) return 0;
+    else return 1;
+}
+
+function numberSortHelper2(first, second) {
+    return first - second;
+}
 
 
 
 // program logic
 const names = [ "John", "Emily", "Michael", "Sophia", "William", "Olivia" ];
 const numbers = [3, 27, 400, 1, 111, 5];
+
+//3, 27, 400, 1, 111, 5
+
+console.table(names);
+names.sort();
+console.table(names);
+
+
+console.table(numbers);
+// use a pre-defined function
+// numbers.sort(numberSortHelper2);
+
+// use an anonymous function
+numbers.sort(function (first, second) {
+    return second - first;
+})
+
+
+console.table(numbers);
+
--- Workbook4/03-advanced-arrays-final/sorting-object-arrays.js
+++ Workbook4/03-advanced-arrays-final/sorting-object-arrays.js
@@ -4,7 +4,27 @@
 
 // program logic
 let products = [
-        {prodId: 2, item: "Notepads (12 pk)", price: 12.29},
+        {prodId: 31, item: "Notepads (12 pk)", price: 12.29},
         {prodId: 12, item: "Black Pens (12 pk)", price: 5.70},
-        {prodId: 22, item: "Stapler", price: 12.79}
+        {prodId: 52, item: "Stapler", price: 12.79}
     ];
+
+console.table(products);
+
+// sort by name asc
+products.sort(function(left, right) {
+    if(left.item <= right.item) return -1;
+    else return 1;
+});
+console.log("Sorted by Name");
+console.table(products);
+
+
+// sort price high to low
+products.sort( (left, right) => { 
+    return right.price - left.price;
+});
+console.log("Sorted by Price H-L");
+console.table(products);
+
+
