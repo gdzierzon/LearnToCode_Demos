b43c1cc0408a825e978c61c2d0427b0b975bfadd
Gregor Dzierzon <gdzierzon@gmail.com>
2023-11-06 14:05:39 -0700

    find and filter demos complete

---
+++ Workbook5/01-array-functions-final/map-convert.js
@@ -0,0 +1,11 @@
+
+let orderItems = [
+    {id: 1, item: "Tacos", category: "Meal", price: 12.29, quantity: 2},
+    {id: 2, item: "Burger", category: "Meal", price: 7.29, quantity: 1},
+    {id: 3, item: "Salad", category: "Meal", price: 8.29, quantity: 3},
+    {id: 5, item: "Coke", category: "Drink", price: 2.29, quantity: 2}
+];
+
+
+
+
