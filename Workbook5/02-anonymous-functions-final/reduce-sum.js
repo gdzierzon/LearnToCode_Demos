528bfcdf989eb63114b612ee2ccbfd27a1b2a185
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-11-07 14:07:54 -0700

    fixed reduce logic error

--- Workbook5/02-anonymous-functions-final/reduce-sum.js
+++ Workbook5/02-anonymous-functions-final/reduce-sum.js
@@ -2,15 +2,14 @@
 let scores = [98, 92, 100, 95, 89, 100, 90];
 
 
-// function addNumber(number, runningTotal) {
+// function addNumber(runningTotal, number) {
 //     return number + runningTotal;
 // }
 
 
 // let sum = scores.reduce(addNumber, 0)
 
-let sum = scores.reduce((number, runningTotal) => number + runningTotal, 0)
-
+let sum = scores.reduce((runningTotal, number) => number + runningTotal, 0)
 
 
 console.log(sum);
