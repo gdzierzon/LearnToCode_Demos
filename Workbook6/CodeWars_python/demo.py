03d3673a41681f2ae4f66280003767cb5cd7464b
Gregor Dzierzon <gdzierzon@gmail.com>
2023-11-27 21:57:06 -0700

    demo for python

---
+++ Workbook6/CodeWars_python/demo.py
@@ -0,0 +1,16 @@
+def sum_mul(n, m):
+    if n  is None or m is None:
+        return 'INVALID'
+    if n <= 0 or m <= 0:
+        return 'INVALID'
+    
+    answer = 0
+    i = n
+    while i < m:
+        answer += i
+        i += n
+    return answer
+
+
+result = sum_mul(2, 9)
+print(result)
