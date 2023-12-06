8bb25c7fbaa48d39966d7bab99bb248ef1c7d583
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-12-06 11:13:04 -0700

    updated parks api data

--- Workbook7/03-national-parks-final/scripts/app.js
+++ Workbook7/03-national-parks-final/scripts/app.js
@@ -1,36 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch("http://localhost:3000/Mountains ")
-        .then(response => response.json())
-        .then(data => {
-            console.table(data)
+    loadData();
 
-            const mountains = data.map( m => {
-                return {
-                    name: m.Name,
-                    elevation: m.Elevation,
-                    effort: m.Effort,
-                    img: m.Img,
-                    desc: m.Desc,
-                    coords: {
-                        lat: m.Coords.Lat,
-                        lng: m.Coords.Lng
-                    }
-                }
-            })
+    // fetch("http://localhost:3000/Mountains ")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.table(data)
 
-            console.table(mountains)
+    //         const mountains = data.map( m => {
+    //             return {
+    //                 name: m.Name,
+    //                 elevation: m.Elevation,
+    //                 effort: m.Effort,
+    //                 img: m.Img,
+    //                 desc: m.Desc,
+    //                 coords: {
+    //                     lat: m.Coords.Lat,
+    //                     lng: m.Coords.Lng
+    //                 }
+    //             }
+    //         })
 
-            displayMountains(mountains)
-        })
+    //         console.table(mountains)
+
+    //         displayMountains(mountains)
+    //     })
 
 })
+
+async function loadData() {
+    let response = await fetch("http://localhost:3000/Mountains ")
+    console.log(response);
+    const data = await response.json()
+    console.table(data)
+
+    const mountains = data.map( m => {
+        return {
+            name: m.Name,
+            elevation: m.Elevation,
+            effort: m.Effort,
+            img: m.Img,
+            desc: m.Desc,
+            coords: {
+                lat: m.Coords.Lat,
+                lng: m.Coords.Lng
+            }
+        }
+    })
+
+    console.table(mountains)
+
+    displayMountains(mountains)
+
+
+}
 
 function displayMountains(mountains)
 {
     const ul = document.getElementById("mountains")
 
     mountains.forEach(moutain => {
+        
         const li = document.createElement("li")
         li.innerText = moutain.name
         ul.appendChild(li)
