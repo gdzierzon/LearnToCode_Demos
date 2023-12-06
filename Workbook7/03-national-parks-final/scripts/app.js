97569418ed04729109bee773905bc5fd076a6627
Gregor Dzierzon <gregor@fullstackdevs.net>
2023-12-06 10:29:30 -0700

    finished api with map demo

---
+++ Workbook7/03-national-parks-final/scripts/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("http://localhost:3000/Mountains ")
+        .then(response => response.json())
+        .then(data => {
+            console.table(data)
+
+            const mountains = data.map( m => {
+                return {
+                    name: m.Name,
+                    elevation: m.Elevation,
+                    effort: m.Effort,
+                    img: m.Img,
+                    desc: m.Desc,
+                    coords: {
+                        lat: m.Coords.Lat,
+                        lng: m.Coords.Lng
+                    }
+                }
+            })
+
+            console.table(mountains)
+
+            displayMountains(mountains)
+        })
+
+})
+
+function displayMountains(mountains)
+{
+    const ul = document.getElementById("mountains")
+
+    mountains.forEach(moutain => {
+        const li = document.createElement("li")
+        li.innerText = moutain.name
+        ul.appendChild(li)
+    });
+
+}
